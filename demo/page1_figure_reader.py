fd32eb634b8f7675ae504ca49d3664593b07a192
Chivier Humber <chivier.humber@outlook.com>
2024-10-07 22:27:41 +0100

    add

--- demo/page1_figure_reader.py
+++ demo/page1_figure_reader.py
@@ -57,9 +57,9 @@ def main():
         model="openrouter/meta-llama/llama-3.2-90b-vision-instruct"
     )
     pixtral_agent = VisionAgent(model="openrouter/mistralai/pixtral-12b")
-    openai_agent.temperature = 0.70
-    llama_agent.temperature = 0.70
-    pixtral_agent.temperature = 0.70
+    openai_agent.temperature = 0.30
+    llama_agent.temperature = 0.30
+    pixtral_agent.temperature = 0.65
     
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
     input_abstract = st.text_input("Enter the abstract of the paper...")
